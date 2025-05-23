2f5a1e27c4521de5d9767986128422dd0e76b279
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-23 12:54:23 -0400

    updated TanstackQuery

--- src/components/TanstackQuery.tsx
+++ src/components/TanstackQuery.tsx
@@ -1,11 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+// const fetchData = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   if (!response.ok) {
+//     throw new Error("Network response was not ok");
+//   }
+//   return response.json();
+// };
 
 const fetchData = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  return response.json();
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+  return response.data;
 };
 
 function TanstackQuery() {
