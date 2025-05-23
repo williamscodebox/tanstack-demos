56b202b2abdc6a0b5b1fb8008d219cbb1c1ed541
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-23 10:04:19 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-600 text-white font-bold text-2xl">
+      App
+    </div>
+  );
+}
+
+export default App;
