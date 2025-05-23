a8e0221dd0f4a8da498e29ceef0bec6756ce0e76
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-23 12:34:23 -0400

    updated TanstackQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import TanstackQuery from "./components/TanstackQuery";
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-600 text-white font-bold text-2xl">
-      App
+    <div className="flex flex-col h-full items-center justify-center bg-gray-600 text-white font-bold text-2xl">
+      <div className="m-2"></div>
+      <div>
+        <TanstackQuery />
+      </div>
     </div>
   );
 }
--- src/components/TanstackQuery.tsx
+++ src/components/TanstackQuery.tsx
@@ -1,10 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 
-const fetchData = async () => {};
+const fetchData = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
 
 function TanstackQuery() {
-  const data = useQuery({ queryKey: ["todo"], queryFn: fetchData });
-  return <div>TanstackQuery</div>;
+  const query = useQuery({ queryKey: ["todo"], queryFn: fetchData });
+  console.log(query);
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-600 text-white font-bold text-2xl">
+      <div className="mb-4">TanstackQuery</div>
+      <div className="border-5 p-10 mb-4">
+        {query.isLoading && "Loading..."}
+        {query.error && `Error: ${(query.error as Error).message}`}
+        {query.data && <pre>{JSON.stringify(query.data, null, 2)}</pre>}
+      </div>
+    </div>
+  );
 }
 
 export default TanstackQuery;
